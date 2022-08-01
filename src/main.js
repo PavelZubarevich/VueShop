b71d21ee644f9bdeca70c2a68af60df599292438
Pavel <pavelzubarevich@gmail.com>
2022-08-01 22:21:40 +0300

    feat: import global components

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Button from 'primevue/button';
+import Message from 'primevue/message';
 
 import 'primevue/resources/themes/arya-orange/theme.css'
 import 'primeicons/primeicons.css'
@@ -9,4 +13,8 @@ import 'primeicons/primeicons.css'
 createApp(App)
   .use(router)
   .use(PrimeVue)
+  .component('InputText', InputText)
+  .component('Password', Password)
+  .component('Button', Button)
+  .component('Message', Message)
   .mount('#app')
